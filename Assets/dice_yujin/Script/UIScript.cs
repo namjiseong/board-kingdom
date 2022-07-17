deb3afb81353778bba46a0848398bd2e65687c42
yujin <yujin2625@gmail.com>
2022-07-17 22:02:01 +0900

    MainUI

---
+++ Assets/dice_yujin/Script/UIScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScript : MonoBehaviour
+{
+    public GameObject MainUI;
+    public GameObject SettingsUI;
+    public void OnClickNewGame()
+    {
+        MainUI.SetActive(false);
+        //새 게임
+    }
+    public void OnClickLoad()
+    {
+        //저장파일 유무
+        //로드파일
+        MainUI.SetActive(false);
+    }
+    public void OnClickSettings()
+    {
+        SettingsUI.SetActive(true);
+    }
+    public void OnClickQuit()
+    {
+        //저장할지 확인 후
+        QuitGame();
+    }
+    void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
